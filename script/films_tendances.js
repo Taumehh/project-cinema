72c15a1d3eb6d8825948343d131ecc47aaa5fc0c
Arthur <arthur.omar@supinfo.com>
2025-01-20 13:46:01 +0100

    Ajout de 5 films tendances
    Changement de l'utilisation de l'api dans la page d'accueil

--- script/films_tendances.js
+++ script/films_tendances.js
@@ -6,38 +6,53 @@ function shuffleArray(array) {
     return array;
 }
 
-async function fetchTrendingMovies(page = 1) {
+async function fetchTrendingMovies() {
     const apiKey = '4640d01a';
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=movie&y=2024&page=${page}`);
-    const data = await response.json();
+    const tendances = ["tt26446278", "tt22022452", "tt27490099", "tt15239678", "tt30795948"];
+    // const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&type=movie&y=2024&p=${currentPage}`);
+    // const data = await response.json();
 
-    if (data.Response === "True") {
-        const shuffledMovies = shuffleArray(data.Search);
+    try {
+        // Effectuer une requête pour chaque imdbID et attendre toutes les réponses
+        const promises = tendances.map(id =>
+            fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`).then(response => response.json())
+        );
+
+        // Récupérer les données des films
+        const movies = await Promise.all(promises);
+
+        // Vérifiez si les films sont valides (Response === "True")
+        const validMovies = movies.filter(movie => movie.Response === "True");
+
+        // Mélanger les films
+        const shuffledMovies = shuffleArray(validMovies);
+
+        // Afficher les films
         displayMovies(shuffledMovies);
-    } else {
-        console.error("Erreur lors de la récupération des films :", data.Error);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des films :", error);
     }
 }
 
 function displayMovies(movies) {
     const container = document.querySelector('#trend-movies');
-    movies.forEach((movie, index) => {
-        setTimeout(() => {
-            container.innerHTML += `
-                <div class="film-card">
-                    <img src="${movie.Poster}" alt="${movie.Title}">
-                    <h3>${movie.Title}</h3>
-                    <a href="movie.html?imdbID=${movie.imdbID}">En savoir plus</a>
-                </div>`;
-        });
+    container.innerHTML = ""; // Nettoyer le conteneur avant d'ajouter les nouveaux films
+    movies.forEach(movie => {
+        container.innerHTML += `
+            <div class="film-card">
+                <img src="${movie.Poster !== "N/A" ? movie.Poster : "img/default-poster.jpg"}" alt="${movie.Title}">
+                <h3>${movie.Title}</h3>
+                <a href="movie.html?imdbID=${movie.imdbID}">En savoir plus</a>
+            </div>`;
     });
 }
 
-let currentPage = 1;
+// let currentPage = 1;
 
-document.querySelector('#load-more').addEventListener('click', () => {
-    currentPage++;
-    fetchTrendingMovies(currentPage);
-});
+// document.querySelector('#load-more').addEventListener('click', () => {
+//     currentPage++;
+//     fetchTrendingMovies(currentPage);
+// });
 
+// Charger les films au démarrage
 fetchTrendingMovies();
